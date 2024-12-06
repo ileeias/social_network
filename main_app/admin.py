a26e98ca61d18e6a46e199ebf134d6b4b2bc5c73
ileeias <iliyas.omarov27@gmail.com>
2024-12-06 13:57:42 +0500

    Delete main_app directory

--- main_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(CustomUser)
-admin.site.register(Friends)
-admin.site.register(Posts)
-admin.site.register(Comments)
-admin.site.register(Likes)
-admin.site.register(Dislikes)
